f55f867571fb2f44cc282763d7514d52e392abd4
chey99 <101052229+it21005008@users.noreply.github.com>
2024-05-13 10:15:31 +0530

    fist commit

---
+++ MovieBooking/MovieBookingApp.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  MovieBooking
+//
+//  Created by chethiya nanayakkara on 2024-01-17.
+//
+
+import SwiftUI
+
+@main
+struct MovieBookingApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
